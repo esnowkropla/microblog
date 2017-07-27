675e8a319738336f875a99545e8d8bf892a4bd57
Elliot Snow-Kropla <esnowkropla@gmail.com>
2017-07-26 20:24:51 -0400

    Got templates working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,24 @@
 extern crate iron;
 extern crate router;
 extern crate staticfile;
+extern crate handlebars_iron;
 
 extern crate microblog;
 
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+use handlebars_iron::{HandlebarsEngine, DirectorySource, Watchable, Template};
 use iron::prelude::Chain;
 use iron::headers::ContentType;
-use iron::{Iron, Handler, status, IronResult, Response, Request};
+use iron::{Set, Iron, Handler, status, IronResult, Response, Request};
 use router::Router;
 use staticfile::Static;
+
 use std::path::Path;
+use std::sync::Arc;
 
 macro_rules! get_http_param {
     ( $r: expr, $e: expr ) => {
@@ -25,13 +34,24 @@ macro_rules! get_http_param {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+struct User {
+    name: String,
+    id: usize,
+}
+
 struct IndexHandler;
 
 impl Handler for IndexHandler {
     fn handle(&self, _: &mut Request) -> IronResult<Response> {
-        let payload = r"<h1>Hello!</h1>";
-        let mut response = Response::with((status::Ok, payload));
+        let x = User {
+            name: String::from("Fred"),
+            id: 13,
+        };
+        let template = Template::new("index", x);
+        let mut response = Response::with(status::Ok);
         response.headers.set(ContentType::html());
+        response.set_mut(template);
         return Ok(response);
     }
 }
@@ -55,18 +75,38 @@ impl Handler for Custom404 {
     }
 }
 
+struct PostHandler;
+
+impl Handler for PostHandler {
+    fn handle(&self, _: &mut Request) -> IronResult<Response> {
+        return Ok(Response::with((status::Ok, "")));
+    }
+}
+
 fn main() {
+    let views_ext = ".hbs";
+    let views_path = "./templates/";
+    let mut hbse = HandlebarsEngine::new();
+    hbse.add(Box::new(DirectorySource::new(views_path, views_ext)));
+    if let Err(e) = hbse.reload() {
+        panic!("{:?}", e.cause);
+    }
+    let hbse_ref = Arc::new(hbse);
+    hbse_ref.watch(views_path);
+
     let mut router = Router::new();
-    //router.get("/", IndexHandler, "index");
+    router.get("/", IndexHandler, "index");
     router.get("/user/:name", UserHandler, "user");
     //router.get("/feed", handlers.feed, "feed");
     //router.post("/post", handlers.make_post, "make_post");
     //router.get("/post/:id", handlers.post, "post");
-    router.get("/", Static::new(Path::new("static/index.html")), "home");
+    //router.get("/", Static::new(Path::new("static/index.html")), "home");
+    router.post("/", PostHandler, "post");
     //router.get("/*", Static::new(Path::new("static/")), "static");
     router.get("*", Custom404, "404");
 
-    let chain = Chain::new(router);
+    let mut chain = Chain::new(router);
+    chain.link_after(hbse_ref);
 
     Iron::new(chain).http("localhost:3000").unwrap();
 }
